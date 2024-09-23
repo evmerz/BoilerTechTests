b8824fc56cb0908371cfe75441fe7f36759b43e0
Emma Patrick <emmapatrick2003@gmail.com>
2024-09-23 14:02:06 -0400

    creating an account actually makes a new row in the db

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: 'database-1.cvmku4284fk0.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Boilerpass0!',
+  database: 'database-1'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+  });
+  
+  // GET request to create a new account
+  app.get('/create-account', (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+  
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Username and password are required' });
+    }
+  
+    const query = 'INSERT INTO users (username, password) VALUES (?, ?)';
+    db.query(query, [username, password], (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: 'Error inserting data into the database' });
+      }
+      res.status(201).json({ message: 'Account created', userId: result.insertId });
+    });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
