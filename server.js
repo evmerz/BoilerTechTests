f706f9739b7490cf4880a82a715e5e86e21c188e
Emma Patrick <emmapatrick2003@gmail.com>
2024-10-28 16:44:24 -0400

    parseing now

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.post('/quiz', async (req, res) => {
 
 
     const query = 'INSERT INTO quiz_submissions (user_id, quiz_id, answers) VALUES (?, ?, ?)';
-      db.query(query, [user_id, quiz_id, JSON.stringify(answers)], (err, result) => {
+      db.query(query, [user_id, quiz_id, JSON.parse(answers)], (err, result) => {
           if (err) {
               return res.status(500).json({ message: 'Error inserting data into the database' });
           }
