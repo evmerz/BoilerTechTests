6f9f5d70a62c2551dcf93f554a89ca0d3db78141
Connor Colbert <cjcolbert159@gmail.com>
2024-10-29 14:44:35 -0400

    added error message printing

--- server.js
+++ server.js
@@ -199,6 +199,7 @@ app.post('/quiz', async (req, res) => {
     const query = 'INSERT INTO quiz_submissions (user_id, quiz_id, answers) VALUES (?, ?, ?)';
       db.query(query, [user_id, quiz_id, answers], (err, result) => {
           if (err) {
+                console.log("error thing: " + err);
                 console.log("ah fuck");
               return res.status(500).json({ message: 'Error inserting data into the database' });
           }
