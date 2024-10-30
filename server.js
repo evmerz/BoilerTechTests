a25240fb72190fe70651dfd8639ee4ea04d869e2
Connor Colbert <cjcolbert159@gmail.com>
2024-10-29 20:27:25 -0400

    set user_id to 0

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.post('/quiz', async (req, res) => {
 
 
     const query = 'INSERT INTO quiz_submissions (user_id, quiz_id, answers) VALUES (?, ?, ?)';
-      db.query(query, [user_id, quiz_id, JSON.stringify(answers)], (err, result) => {
+      db.query(query, [0, quiz_id, JSON.stringify(answers)], (err, result) => {
           if (err) {
                 console.log("error thing: ", err);
                 console.log("ah fuck");
