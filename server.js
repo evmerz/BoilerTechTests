cfe8823be84ef810bde72ef72103bcde4649611e
Emma Patrick <emmapatrick2003@gmail.com>
2024-11-01 01:17:53 -0400

    !submitted

--- server.js
+++ server.js
@@ -215,7 +215,7 @@ app.post('/quiz', async (req, res) => {
         }
     })
     
-    if (submitted) {
+    if (!submitted) {
         const query = 'INSERT INTO quiz_submissions (user_id, quiz_id, answers) VALUES (?, ?, ?)';
         db.query(query, [user_id, quiz_id, JSON.stringify(answers)], (err, result) => {
             if (err) {
