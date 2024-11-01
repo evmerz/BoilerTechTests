a4e015a67ed52926aa6e227bc366b115c1877164
Emma Patrick <emmapatrick2003@gmail.com>
2024-11-01 01:14:06 -0400

    checking if previously submitted

--- server.js
+++ server.js
@@ -205,30 +205,37 @@ app.post('/quiz', async (req, res) => {
     // } 
 
     const checkQuery = 'SELECT * from quiz_submissions WHERE (user_id, quiz_id) = (?, ?)';
+    const submitted = false;
     db.query(checkQuery, [user_id, quiz_id], (err, result) =>{
         if (err) {
             return res.status(500).json({ message: 'Error retrieving data from the database' });
         }
         if (result.length > 0) {
-            return res.status(400).json({ message: 'You have already submitted this quiz' });
-            query = 'UPDATE quiz_submissions SET answers = ? WHERE (user_id, quiz_id) = (?, ?)'
-        } else {
-            query = 'INSERT INTO quiz_submissions (user_id, quiz_id, answers) VALUES (?, ?, ?)';
+            submitted = true;
         }
-
     })
-
-
-     query = 'INSERT INTO quiz_submissions (user_id, quiz_id, answers) VALUES (?, ?, ?)';
-      db.query(query, [user_id, quiz_id, JSON.stringify(answers)], (err, result) => {
-          if (err) {
-                console.log("error thing: ", err);
-                console.log("ah fuck");
-                console.log("result: ", result);
+    
+    if (submitted) {
+        const query = 'INSERT INTO quiz_submissions (user_id, quiz_id, answers) VALUES (?, ?, ?)';
+        db.query(query, [user_id, quiz_id, JSON.stringify(answers)], (err, result) => {
+            if (err) {
+                    console.log("error thing: ", err);
+                    console.log("ah fuck");
+                    console.log("result: ", result);
+                    return res.status(500).json({ message: 'Error inserting data into the database' });
+            }
+            res.status(201).json({ message: 'Quiz submission saved!'});
+        });
+    } else {
+        const updateQuery = 'UPDATE quiz_submissions SET answers = ? WHERE (user_id, quiz_id) = (?, ?)';
+        db.query(updateQuery, [JSON.stringify(answers), user_id, quiz_id], (err, result) => {
+            if (err) {
                 return res.status(500).json({ message: 'Error inserting data into the database' });
-          }
-          res.status(201).json({ message: 'Quiz submission saved!'});
-      });
+            }
+            res.status(201).json({ message: 'Quiz submission saved!'});
+        });
+    }
+    
 });
 
 app.get('/get-submit', (req, res) => {
